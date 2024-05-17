5515d37d0b022f34b628ae09d136fcf73b511fb2
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-18 00:42:24 +0600

    Shelves  SQL ready

---
+++ Cleaned data/shelves.sql
@@ -0,0 +1,12 @@
+INSERT INTO Shelves (Location, Type) 
+VALUES 
+('1st Floor', 'Wooden'),
+('1st Floor', 'Steel'),
+('2nd Floor', 'Wooden'),
+('2nd Floor', 'Steel'),
+('3rd Floor', 'Wooden'),
+('3rd Floor', 'Steel'),
+('1st Floor', 'Wooden'),
+('2nd Floor', 'Steel'),
+('3rd Floor', 'Wooden'),
+('1st Floor', 'Steel');
