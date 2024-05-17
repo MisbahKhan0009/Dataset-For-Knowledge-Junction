2c735f73609c7b922d2503493d73d70eacb92667
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-17 06:48:43 +0600

    SQL files for tables.

---
+++ Cleaned data/language.sql
@@ -0,0 +1,3 @@
+INSERT INTO `Languages` (`Name`,`Notes`)
+VALUES
+('English','English is an international language spoken by millions of people worldwide, making it one of the most widely used languages for communication, literature and education.');
