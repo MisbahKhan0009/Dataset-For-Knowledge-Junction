4a005175c9609290fdefec6327351ce295ed6715
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-18 00:41:55 +0600

    Issue Receiver SQL ready

---
+++ Cleaned data/Issue receiver.sql
@@ -0,0 +1,4 @@
+INSERT INTO Book_Issuer_Receiver (Name, Designation, Qualification) VALUES
+('John wick', 'Librarian', 'Master of Library Science'),
+('Harry Potter', 'Assistant Librarian', 'Bachelor of Arts in English Literature');
+
